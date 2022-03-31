dbf52879e56b3122d85e5de20a5f085a6001891d
michael-prye <michael.prye@gmail.com>
2022-03-31 13:01:27 -0400

    Created new view function for getting single product by ID + Created url path for the view function

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.get_products)
+    path('', views.get_products),
+    path('<int:pk>/', views.get_single_product),
 ]
--- products/views.py
+++ products/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import ProductSerializer
 from .models import Product
+from products import serializers
 
 
 @api_view(['GET', 'POST'])
@@ -16,4 +17,13 @@ def get_products(request):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-      
+
+@api_view(['GET'])
+def get_single_product(request, pk):
+    try:
+        query_set= Product.objects.get(pk=pk)
+        serializer = ProductSerializer(query_set)
+        return Response(serializer.data)
+    except Product.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
