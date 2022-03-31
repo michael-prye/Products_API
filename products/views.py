f67d91caff451570a44aa350385a80fd457c1cc9
michael-prye <michael.prye@gmail.com>
2022-03-31 12:44:52 -0400

    Completed post function in views.py

--- products/views.py
+++ products/views.py
@@ -1,8 +1,19 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .serializers import ProductSerializer
+from .models import Product
 
-@api_view(['GET'])
+
+@api_view(['GET', 'POST'])
 def get_products(request):
-
-
-    return Response('OK')
+    if request.method == 'GET':
+        query_set = Product.objects.all()
+        serializer = ProductSerializer(query_set, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+      
