a5f619ec76f7f6ddec0a7fce9953a32829385fd7
michael-prye <michael.prye@gmail.com>
2022-04-01 12:32:34 -0400

    Updated models.py in Products app, Added Review FK app

--- products/models.py
+++ products/models.py
@@ -1,4 +1,7 @@
+from asyncio.windows_events import NULL
+from turtle import ontimer
 from django.db import models
+from reviews.models import Review
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
@@ -6,3 +9,4 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2)
     inventory_quantity = models.IntegerField()
     image_link = models.CharField(max_length=255)
+    product_review = models.ForeignKey(Review, on_delete=models.CASCADE)
